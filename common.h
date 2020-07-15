aab84d5ea96f2cfe18885663acd5b645d94bfa22
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-16 00:56:30 +0300

    commong modules and constants added

---
+++ common.h
@@ -0,0 +1,18 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define TABLE_MAX_PAGES 100
+
+#endif
+
